6038ffe1063b79655258032f74219ef5f0d8e517
Huy Lee <leduchuy13t2@gmail.com>
2020-05-20 23:48:29 +0700

    [new] - add mores packages redux, router, ant-design, scss

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.scss';
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+       <div>Home</div>
+    );
+  }
+}
+
+export default Home;
